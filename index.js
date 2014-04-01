fb15bb5c297ec304129bb108e435c2a053f20b90
Andy Joslin <andytjoslin@gmail.com>
2014-03-31 18:18:43 -0600

    fix get

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-var fs = require('fs');
 var request = require('request');
-var lingo = {};
-
-fs.readdirSync(__dirname + '/templates').forEach(function(fname) {
-  var contents = fs.readFileSync(__dirname + '/templates/' + fname).toString();
-  lingo[fname.replace(/.md$/, '')] = contents;
-});
 
 module.exports = {
   get: function(name, cb) {
     request(
-      'http://rawgithub.com/ionitron-lingo/master/templates/' + name + '.md',
+      'https://raw.github.com/driftyco/ionitron-lingo/master/templates/' + name + '.md',
       function(err, response, body) {
-        if (err || response.status !== 200) return cb(err);
+        if (err) return cb(true);
         cb(null, body);
       }
     );
