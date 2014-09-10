88b9414e10d0a729e80695c8fbe7be5e745a6041
Adam Bradley <adambradley25@gmail.com>
2014-09-09 21:13:23 -0500

    allow custom message when using double quotes

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ var request = require('request');
 
 module.exports = {
   get: function(name, cb) {
+
+    var customMessage = name.match( /"(.*?)"/ );
+
+    if( customMessage && customMessage.length > 1 ) {
+      // if the name is surrounded by double quotes then its a
+      // custom message and not a template
+      cb(null, customMessage[1]);
+      return;
+    }
+
     request(
       'https://raw.github.com/driftyco/ionitron-lingo/master/templates/' + name + '.md',
       function(err, response, body) {
